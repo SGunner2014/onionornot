14540b8441afdf15551434e84f4a3ea66e55cfbd
Sam Gunner <samgunnerpc@live.com>
2015-06-16 22:51:49 +0100

    Updated - Spits out title in correct format

--- main.php
+++ main.php
@@ -1,8 +1,23 @@
 <?php
 
-$feed = implode(file('http://reddit.com/r/theonion.rss'));
+$feed = implode(file('http://reddit.com/r/nottheonion.rss'));
 $xml = simplexml_load_string($feed);
 $json = json_encode($xml);
 $array = json_decode($json,TRUE);
+print_r($array['channel']);
+
+function getSub($arr) {
+	$link = substr($arr['link'], 24, 24);
+	if (strtolower($link) === 'n') {
+		return 'notonion';
+	} else {
+		return 'onion';
+	}
+}
+
+foreach($array['channel']['item'] as $art) {
+	$sub = getSub($art);
+	echo '<p id="'.$sub.'">'.$art['title'].'</p>';
+}
 
 ?>
