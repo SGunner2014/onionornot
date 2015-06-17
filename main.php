10f220e350bd86bd0988c269514c419eb5bb7dbe
Sam Gunner <samgunnerpc@live.com>
2015-06-17 09:16:48 +0100

    Random Updates

--- main.php
+++ main.php
@@ -1,13 +1,15 @@
+<meta charset="UTF-8">
+
 <?php
 
 $feed = implode(file('http://reddit.com/r/nottheonion.rss'));
 $xml = simplexml_load_string($feed);
 $json = json_encode($xml);
 $array = json_decode($json,TRUE);
-print_r($array['channel']);
+//print_r($array['channel']);
 
 function getSub($arr) {
-	$link = substr($arr['link'], 24, 24);
+	$link = substr($arr['link'], 24, 1);
 	if (strtolower($link) === 'n') {
 		return 'notonion';
 	} else {
@@ -15,9 +17,20 @@ function getSub($arr) {
 	}
 }
 
+function printSub() {
+	$tmpArray = $array['channel']['item'];
+	shuffle($tmpArray);
+	$part = $tmpArray[1];
+	$sub = getSub($part);
+	echo '<p id="'.$sub.'">'.$part['title'].'</p>';
+}
+
+/*
 foreach($array['channel']['item'] as $art) {
 	$sub = getSub($art);
 	echo '<p id="'.$sub.'">'.$art['title'].'</p>';
 }
+*/
+
 
 ?>
