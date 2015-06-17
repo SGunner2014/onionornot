9d49da347a96a338420d7df8cf1d4e1a9ceb987e
Sam Gunner <samgunnerpc@live.com>
2015-06-17 08:44:56 +0100

    Updated - Added script to detect right or wrong answer

---
+++ scripts/oornot.js
@@ -0,0 +1,28 @@
+/*
+*	This script compares the choice they have made and tests to see whether they have chosen the correct one.
+*	If so, it will make a window popup, saying that they have chosen the correct one, and if not, do the reverse.
+*/
+
+$(document).ready(function() {
+
+	$('#false').click(function() {
+
+		if ( $('#onion').length > 1 ) { //Chosen the correct choice, it is the onion.
+			$('.correctAnswer').slideDown();
+		} else {
+			$('.wrongAnswer').slideDown();
+		}
+
+	});
+
+	$('#true').click(function() {
+
+		if ( $('#notonion').length > 1 ) {
+			$('.correctAnswer').slideDown();
+		} else {
+			$('.wrongAnswer').slideDown();
+		}
+
+	});
+
+});
